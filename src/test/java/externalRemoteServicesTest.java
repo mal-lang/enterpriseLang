ad478105ceba92c504c406b6f4b78b0a2e807261
Oscar <oscarabergg@gmail.com>
2019-06-12 12:01:04 +0200

    tests added for External Remote Services

---
+++ src/test/java/externalRemoteServicesTest.java
@@ -0,0 +1,48 @@
+import org.antlr.runtime.tree.Tree;
+import org.junit.Test;
+
+import core.*;
+import attack.*;
+
+public class externalRemoteServicesTest{
+
+  @Test
+  public void test1() {
+    // Section 1: Asset instantiation
+    User user = new User("user");
+    ExternalNetwork externalNetwork = new ExternalNetwork("externalNetwork");
+    PrivateNetwork privateNetwork = new PrivateNetwork("privateNetwork");
+
+
+    // Section 2: Asset connections and attacker creation
+    user.addExternalNetwork(externalNetwork);
+    externalNetwork.addPrivateNetwork(privateNetwork);
+
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(user.connectToVpn);
+    attacker.attack();
+
+    // Section 3: Assertions
+    privateNetwork.vpnInternalConnection.assertCompromisedInstantaneously();
+  }
+
+  @Test
+  public void test2() {
+    // Section 1: Asset instantiation
+    User user = new User("user");
+    ExternalNetwork externalNetwork = new ExternalNetwork("externalNetwork",false,true);
+    PrivateNetwork privateNetwork = new PrivateNetwork("privateNetwork");
+
+
+    // Section 2: Asset connections and attacker creation
+    user.addExternalNetwork(externalNetwork);
+    externalNetwork.addPrivateNetwork(privateNetwork);
+
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(user.connectToVpn);
+    attacker.attack();
+
+    // Section 3: Assertions
+    privateNetwork.vpnInternalConnection.assertUncompromised();
+  }
+}
