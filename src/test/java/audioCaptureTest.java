caf3e6082ca7f553e578a5f6619851e102b06a99
Oscar <oscarabergg@gmail.com>
2019-07-15 14:11:40 +0200

    fix audio capture and its tests

---
+++ src/test/java/audioCaptureTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import core.*;
+import jdk.internal.jline.internal.TestAccessible;
+import attack.*;
+
+public class audioCaptureTest{
+
+    @Test
+    public void test1(){
+        User user = new User("user");
+        Computer computer = new Computer("computer", true,false,false);
+        Microphone microphone = new Microphone("microphone");
+
+        user.addComputer(computer);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(user.userRights);
+        attacker.attack();
+
+        computer.dataCollected.assertUncompromised();
+
+    }
+
+    public void test2(){
+        User user = new User("user");
+        Computer computer = new Computer("computer");
+        Microphone microphone = new Microphone("microphone");
+
+        user.addComputer(computer);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(user.userRights);
+        attacker.attack();
+
+        computer.dataCollected.assertCompromisedInstantaneously();
+
+    }
+    public void test3(){
+        User user = new User("user");
+        Computer computer = new Computer("computer");
+   
+
+        user.addComputer(computer);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(user.userRights);
+        attacker.attack();
+
+        computer.dataCollected.assertUncompromised();
+
+    }
+}
