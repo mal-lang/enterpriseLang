be66b63eae4df5e9d25b3dfc2430be207f0ed3cd
Oscar <oscarabergg@gmail.com>
2019-07-19 16:14:59 +0200

    AppleScript

---
+++ individualMavenTest/appleScript/src/test/java/appleScriptTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import core.*;
+import attack.*;
+
+public class appleScriptTest {
+
+  @Test
+  public void appleScriptTestTestNoDefens() {
+    MacOS mac = new MacOS("mac");
+    User user = new User("user");
+
+    user.addMacOS(mac);
+
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(user.userRights);
+    attacker.attack();
+    
+    mac.runCode.assertCompromisedInstantaneously();
+  }
+
+  @Test
+  public void appleScriptTestTestWithDefens() {
+    MacOS mac = new MacOS("mac",true);
+    User user = new User("user");
+
+    user.addMacOS(mac);
+
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(user.userRights);
+    attacker.attack();
+    
+    mac.runCode.assertUncompromised();
+  }
+}
