049705271ff863970e380b2ae77d37ab426e2e8a
Oscar <oscarabergg@gmail.com>
2019-07-22 10:56:36 +0200

    bash history

---
+++ individualMavenTest/bashHistory/src/test/java/bashHistoryTest.java
@@ -0,0 +1,81 @@
+import org.antlr.runtime.tree.Tree;
+import org.junit.Test;
+
+import core.*;
+import attack.*;
+
+public class bashHistoryTest {
+
+  @Test
+  public void linuxTestNoDefense() {
+    User user = new User("user");
+    Computer computer = new Computer("computer");
+    Linux linux= new Linux("linux");
+
+    user.addLinux(linux);
+    computer.addLinux(linux);
+
+    // Attack  
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(computer.infectedComputer);
+    attacker.attack();
+
+    //linux.bashHistory.assertCompromisedInstantaneously();
+    user.userCredentials.assertCompromisedInstantaneously();
+    user.adminLogin.assertCompromisedInstantaneously();
+  }
+  
+  @Test
+  public void macOSTestNoDefense() {
+    User user = new User("user");
+    Computer computer = new Computer("computer");
+    MacOS macOS= new MacOS("macOS");
+
+    user.addMacOS(macOS);
+    computer.addMacOS(macOS);
+
+    // Attack  
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(computer.infectedComputer);
+    attacker.attack();
+
+    user.userCredentials.assertCompromisedInstantaneously();
+    user.adminLogin.assertCompromisedInstantaneously();
+  }
+  @Test
+  public void linuxTestWithDefense() {
+    User user = new User("user");
+    Computer computer = new Computer("computer");
+    Linux linux= new Linux("linux",true);//turnOfBashHistory in win is the defens
+
+    user.addLinux(linux);
+    computer.addLinux(linux);
+
+    // Attack  
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(computer.infectedComputer);
+    attacker.attack();
+
+    //linux.bashHistory.assertCompromisedInstantaneously();
+    user.userCredentials.assertUncompromised();
+    user.adminLogin.assertUncompromised();
+  }
+  
+  @Test
+  public void macOSTestwithDefense() {
+    User user = new User("user");
+    Computer computer = new Computer("computer");
+    MacOS macOS= new MacOS("macOS",true);
+
+    user.addMacOS(macOS);
+    computer.addMacOS(macOS);
+
+    // Attack  
+    Attacker attacker = new Attacker();
+    attacker.addAttackPoint(computer.infectedComputer);
+    attacker.attack();
+
+    user.userCredentials.assertUncompromised();
+    user.adminLogin.assertUncompromised();
+  }
+}
