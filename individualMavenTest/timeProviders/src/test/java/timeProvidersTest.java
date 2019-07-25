889e616a65931aef1525fddad17cd2c48a522997
Oscar <oscarabergg@gmail.com>
2019-07-25 11:54:52 +0200

    Added Time Providers and tests.

---
+++ individualMavenTest/timeProviders/src/test/java/timeProvidersTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import core.*;
+import attack.*;
+
+public class timeProvidersTest{
+
+    @Test
+    public void test1(){
+        // Section 1: Asset instantiation
+        Administrator administrator = new Administrator("administrator");
+        Windows windows = new Windows("windows", true);
+
+        // Section 2: Asset connections and attacker creation
+        administrator.addWindows(windows);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(administrator.adminRights);
+        attacker.attack();
+
+        // Section 3: Assertions
+        windows.persistence.assertUncompromised();
+
+    }
+    @Test
+    public void test2(){
+        // Section 1: Asset instantiation
+        Administrator administrator = new Administrator("administrator");
+        Windows windows = new Windows("windows");
+
+        // Section 2: Asset connections and attacker creation
+        administrator.addWindows(windows);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(administrator.adminRights);
+        attacker.attack();
+
+        // Section 3: Assertions
+        windows.persistence.assertCompromisedInstantaneously();
+        
+    }
+}
