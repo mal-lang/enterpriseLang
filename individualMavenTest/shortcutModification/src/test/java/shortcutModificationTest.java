ae2f58b026820f9a479153802a3079fd08d8c704
Oscar <oscarabergg@gmail.com>
2019-08-02 14:39:31 +0200

    Added Shortcut Modification and tests

---
+++ individualMavenTest/shortcutModification/src/test/java/shortcutModificationTest.java
@@ -0,0 +1,76 @@
+import org.junit.Test;
+
+import core.*;
+import attack.*;
+
+public class shortcutModificationTest{
+
+    @Test
+    public void test1(){
+        // Section 1: Asset instantiation
+        Administrator administrator = new Administrator("administrator");
+        Windows windows = new Windows("windows", true);
+
+        // Section 2: Asset connections and attacker creation
+        administrator.addWindows(windows);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(administrator.adminRights);
+        attacker.attack();
+
+        // Section 3: Assertions
+        windows.persistence.assertUncompromised();
+
+    }
+    @Test
+    public void test2(){
+        // Section 1: Asset instantiation
+        Administrator administrator = new Administrator("administrator");
+        Windows windows = new Windows("windows");
+
+        // Section 2: Asset connections and attacker creation
+        administrator.addWindows(windows);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(administrator.adminRights);
+        attacker.attack();
+
+        // Section 3: Assertions
+        windows.persistence.assertCompromisedInstantaneously();
+        
+    }
+    @Test
+    public void test3(){
+        // Section 1: Asset instantiation
+        User user = new User("user");
+        Windows windows = new Windows("windows", true);
+
+        // Section 2: Asset connections and attacker creation
+        user.addWindows(windows);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(user.userRights);
+        attacker.attack();
+
+        // Section 3: Assertions
+        windows.persistence.assertUncompromised();
+
+    }
+    @Test
+    public void test4(){
+        // Section 1: Asset instantiation
+        User user = new User("user");
+        Windows windows = new Windows("windows");
+
+        // Section 2: Asset connections and attacker creation
+        user.addWindows(windows);
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(user.userRights);
+        attacker.attack();
+
+        // Section 3: Assertions
+        windows.persistence.assertCompromisedInstantaneously();
+        
+    }
+}
